e287ded8d961c420399592d7674e43c6b0aa67da
jakke <itsjakk3@gmail.com>
2023-08-11 17:31:27 +0900

    first commit

---
+++ src/Study_WPFSQLite/DB/DBMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace StudyWPFSQLite.DB
+{
+    public static class DBMain
+    {
+        public static void GetSQLiteVersion()
+        {
+            string connStr = @"Data Source = D:\MySQLite\Study\HelloWorld\mysqlite.db";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            var con = new SQLiteConnection(connStr);
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            
+        }
+    }
+}
